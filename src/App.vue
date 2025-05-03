ea1ac1e8bcdfec525a4bf00abf1d6909f3fa6f21
Zhanna <kezhanna.03@mail.ru>
2025-05-04 02:51:10 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <FixedHeader/>
+    <RouterView/>
+  </div>
+</template>
+
+<script>
+import FixedHeader from './components/Header.vue'
+import { RouterView } from "vue-router";
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import FixedHeader from '..//components/Header.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: FixedHeader,
+    },
+  ],
+})
+
+export default router
